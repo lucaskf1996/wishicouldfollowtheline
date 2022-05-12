261a9231ef3f183fadd131c296ba81a92b288be3
Bilbia <biamcs2000@gmail.com>
2022-05-12 18:49:48 -0300

    tirando alguns assets n usados

--- wishicouldfollowtheline/Assets/Scripts/TaskManager.cs
+++ wishicouldfollowtheline/Assets/Scripts/TaskManager.cs
@@ -8,19 +8,21 @@ public class TaskManager : MonoBehaviour
     public Text taskText;
     
     GameManager gm;
+    AudioSource aS;
 
     void Start()
     {
         gm = GameManager.GetInstance();
+        aS = GetComponent<AudioSource>();
     }
 
     public IEnumerator StartTask(string task){
         yield return new WaitUntil(() => gm.inDialogue == false);
         Debug.Log("Starting task");
         taskText.gameObject.SetActive(true);
-
         taskText.text = task;
         Debug.Log(task);
+        aS.Play(0);
     }
 
     public void EndTask(){
