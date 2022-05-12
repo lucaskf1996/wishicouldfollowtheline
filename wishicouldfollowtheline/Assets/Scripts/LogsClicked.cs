10dc5727ff049cb4bbc7d1ba90c21237172dd032
lucaskf1996 <email@example.com>
2022-05-12 19:38:38 -0300

    audios

--- wishicouldfollowtheline/Assets/Scripts/LogsClicked.cs
+++ wishicouldfollowtheline/Assets/Scripts/LogsClicked.cs
@@ -7,15 +7,33 @@ public class LogsClicked : MonoBehaviour
     public GameObject axe;
     GameManager gm;
     private int count = 0;
+    public AudioClip[] audios;
+    private AudioSource source;
+    // TaskManager tm;
+    // DialogueTrigger startDialogue;
+    // TaskTrigger startTask;
 
     void Start(){
         gm = GameManager.GetInstance();
+        source = GetComponent<AudioSource>();
+        source.clip = audios[0];
+        // tm = FindObjectOfType<TaskManager>();
+        // startDialogue = GetComponent<DialogueTrigger>();
+        // startTask = GetComponent<TaskTrigger>();
     }
 
     void Clicked(){
         if(axe.activeSelf && gm.waterPlaced){
             if(count < 2){
-                GetComponent<AudioSource>().Play();
+                source.Play();
+                count++;
+            }
+            if(count == 2){
+                source.clip = audios[1];
+                source.Play();
+                // tm.EndTask();
+                // startDialogue.TriggerDialogue();
+                // startTask.TriggerTask();
             }
         }
     }
